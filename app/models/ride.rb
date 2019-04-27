4ed64c95b483685bd7b688ddfa18751933038896
github username <github email address>
2019-04-27 18:04:01 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,30 @@
 class Ride < ActiveRecord::Base
+    belongs_to :attraction
+    belongs_to :user
+
+    def take_ride
+        error = ""
+
+        if self.user.tickets < self.attraction.tickets
+            error += "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+        else 
+            self.user.tickets -= self.attraction.tickets 
+        end 
+        if self.user.height < self.attraction.min_height
+            if error == ""
+                error += "Sorry. You are not tall enough to ride the #{attraction.name}."
+            else 
+                error += " You are not tall enough to ride the #{attraction.name}."
+            end 
+        end 
+
+        if error == ""
+            self.user.happiness += self.attraction.happiness_rating
+            self.user.nausea += self.attraction.nausea_rating
+            self.user.save
+        end 
+
+        error
+
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new, :create, :show]
+  resources :sessions, only: [:create]
+  resources :attractions, only: [:index, :show, :new, :create, :edit, :update]
+  get "/signin", to: "sessions#new", as: "new_session"
+  get "/logout", to: "sessions#destroy", as: "logout"
+  post "/attractions/:id", to: "attractions#ride", as: "ride"
+  
+  get "/", to: "application#home", as: "root"
 end
