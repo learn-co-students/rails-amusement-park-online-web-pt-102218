4697a1b83f6eca1ed62fb1382438e116d4fef426
github username <github email address>
2019-04-26 19:23:50 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :rides
+    has_many :rides, through: :attractions
+
+    #create rides and attractions tables to be able to have assocations.
 end
