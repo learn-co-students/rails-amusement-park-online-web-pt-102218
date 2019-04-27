de07d96d8ccb16b7450ab70e39117b432cf6cdcf
github username <github email address>
2019-04-26 23:21:34 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :rides
-    has_many :rides, through: :attractions
+    has_many :attractions, through: :rides
 
-    #create rides and attractions tables to be able to have assocations.
+
+
+    def mood
+        if self.nausea > self.happiness
+            mood = "sad"
+        else
+            mood = "happy"
+        end
+    end
 end
