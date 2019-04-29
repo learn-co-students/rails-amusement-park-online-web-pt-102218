12e7e52655c591caa8c1a8a8e813d1e75645a695
Johnathon Wood <jaywood128@gmail.com>
2019-04-28 20:02:31 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -8,8 +8,12 @@ class Ride < ActiveRecord::Base
         if u.tickets >= a.tickets && u.height >= a.min_height 
             u.tickets = u.tickets - a.tickets 
             u.save 
+        elsif u.tickets < a.tickets && u.height < a.min_height
+            "Sorry. You do not have enough tickets to ride the #{a.name}. You are not tall enough to ride the #{a.name}."
+        elsif u.height < a.min_height
+            "Sorry. You are not tall enough to ride the #{a.name}."
         elsif u.tickets < a.tickets 
-            "Sorry. You do not have enough tickets to ride the #{a.name}." 
+            "Sorry. You do not have enough tickets to ride the #{a.name}."
         end 
     end 
 end
