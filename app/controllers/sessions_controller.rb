f22fa9e7da0c91ea6d22ead5b2ab6573cc3624e9
github username <github email address>
2019-04-29 00:32:06 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.find(params[:user][:id])
+        @user = User.find(params[:user][:name])
         session[:user_id] = @user.id
         redirect_to user_path(@user)
     end
 
     def destroy
-        session.clear
+        session.destroy
         redirect_to '/'
     end
 end
