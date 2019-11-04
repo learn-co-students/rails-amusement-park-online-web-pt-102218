5071a9d8b2e70e9bf8d1160ef50555434fdbef43
beaulieu527 <beaulieudev@gmail.com>
2019-11-04 18:36:26 -0500

    Done.

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,64 @@
+class AttractionsController < ApplicationController
+
+    def index
+        @attractions = Attraction.all
+        @current_user = current_user
+    end 
+
+    def show
+        @attraction = Attraction.find(params[:id])
+        @current_user = current_user
+    end 
+
+    def ride
+        attraction = Attraction.find_by(id: params[:id])
+        if session.has_key?(:user_id) && attraction
+            ride = Ride.new
+            ride.user = current_user
+            ride.attraction = attraction
+            response = ride.take_ride
+            ride.save
+
+            if response == ""
+                redirect_to user_path(current_user), notice: "Thanks for riding the #{attraction.name}!"
+            else 
+                redirect_to user_path(current_user), alert: response
+            end 
+        end 
+    end 
+
+    def new
+        if current_user.admin 
+            @attraction = Attraction.new
+        else
+            redirect_to root_path
+        end 
+    end 
+
+    def create
+        if current_user.admin 
+            @attraction = Attraction.create(attraction_params)
+            redirect_to attraction_path(@attraction)
+        else
+            redirect_to root_path
+        end
+    end 
+
+    def edit
+        if current_user.admin
+            @attraction = Attraction.find_by(id: params[:id])
+        else 
+            redirect_to root_path
+        end 
+    end 
+
+    def update
+        @attraction = Attraction.find_by(id: params[:id])
+        @attraction.update(attraction_params)
+        redirect_to attraction_path(@attraction)
+    end
+
+    def attraction_params
+        params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
+    end ttractionsController < ApplicationController
+end
